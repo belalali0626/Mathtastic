aa15cbdb55833e5dda7337a196da027e4752b388
belalali0626 <1915996@brunel.ac.uk>
2022-02-03 02:33:13 +0000

    New Map + Movement

---
+++ Assets/Scripts/PlayerControllerMB.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerMB : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    public Animator Anim;
+
+    // Storing movement, horizontal and vertical 
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Animator
+        Anim.SetFloat("Horizontal", movement.x);
+        Anim.SetFloat("Vertical", movement.y);
+        Anim.SetFloat("Speed", movement.sqrMagnitude);
+
+
+    }
+
+    private void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+
+
+    }
+}
