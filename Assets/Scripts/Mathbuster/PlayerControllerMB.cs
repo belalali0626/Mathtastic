7dcdeb5948765399ea033c141e7a6ac235aa036b
Belal Ali <77330921+belalali0626@users.noreply.github.com>
2022-02-18 20:36:23 +0000

    Multiplayer is now functionable and synced

---
+++ Assets/Scripts/Mathbuster/PlayerControllerMB.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class PlayerControllerMB : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public Animator Anim;
+
+    //Photon Stuff
+    PhotonView view;
+
+    // crosshair for shooting   
+    public GameObject crosshair;
+    public bool endOfAiming;
+
+
+    //Prefabs
+    public GameObject shuriken;
+
+
+    // Storing movement, horizontal and vertical 
+    public float moveSpeed = 5f;
+    public float shu_base_speed = 1.0f;
+    Vector2 movement;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!view.IsMine)
+            return;
+        
+        Aim();
+        ProcessInput();
+        Shoot();
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Animator
+        Anim.SetFloat("Horizontal", movement.x);
+        Anim.SetFloat("Vertical", movement.y);
+        Anim.SetFloat("Speed", movement.sqrMagnitude);
+        
+        
+       
+
+    }
+    
+    private void FixedUpdate() 
+    {
+
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
+
+
+    }
+
+    void ProcessInput()
+    {
+        endOfAiming = Input.GetButtonUp("Fire1");
+    }
+
+    void Aim()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        crosshair.transform.localPosition = mousePos;
+
+     }
+
+    void Shoot()
+    {
+        Vector2 ShootingDirection = crosshair.transform.localPosition;
+        ShootingDirection.Normalize();
+
+
+        if (endOfAiming)
+        {
+            Debug.Log("Hii");
+            GameObject shu = Instantiate(shuriken, transform.position, Quaternion.identity);
+            shu.GetComponent<Rigidbody2D>().velocity = ShootingDirection * shu_base_speed;
+            shu.transform.Rotate(0, 0, Mathf.Atan2(ShootingDirection.y, ShootingDirection.x) * Mathf.Rad2Deg);
+            //Destroy(shu, 2.0f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Mathbuster/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PhoView;
+
+    private void Awake()
+    {
+        PhoView = GetComponent<PhotonView>();
+    }
+    void Start()
+    {
+        if(PhoView.IsMine)
+        {
+            CreateController();
+        }
+        
+    }
+
+    void CreateController()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Player"), Vector3.zero, Quaternion.identity);
+
+    }
+}
