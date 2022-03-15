33c4755c37c90429c865fa940b23657ab8a943cc
Belal Ali <77330921+belalali0626@users.noreply.github.com>
2022-03-15 03:59:03 +0000

    Implemented countdown + fixed disconnect bug + other bugs

--- Assets/Scripts/Rambo/ServerHandler.cs
+++ Assets/Scripts/Rambo/ServerHandler.cs
@@ -5,15 +5,54 @@ using UnityEngine;
 
 public class ServerHandler : MonoBehaviourPunCallbacks
 {
+    public int index;
     public void Leave()
     {
         Time.timeScale = 1;
         PhotonNetwork.Disconnect();
     }
 
+    public void DisconnectPlayer()
+    {
+        StartCoroutine(DiscAndLoad());
+    }
+    
+    public void LeaveLobby()
+    {
+        StartCoroutine(QuitGame());
+
+    }
     public override void OnLeftRoom()
     {
-        PhotonNetwork.LoadLevel(0);
+        PhotonNetwork.LoadLevel(index);
+        base.OnLeftRoom();
     }
+
+
+    IEnumerator DiscAndLoad()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.Disconnect();
+
+        while (PhotonNetwork.InRoom)
+            yield return null;
+        PhotonNetwork.LoadLevel(0);
+        Destroy(GameObject.Find("DontDestroyOnLoad"));
+        Destroy(GameObject.Find("RoomManager"));
+
+
+    }
+
+    IEnumerator QuitGame()
+    {
+        PhotonNetwork.LeaveRoom();
+        while (PhotonNetwork.InRoom)
+            yield return null;
+        Destroy(GameObject.Find("DontDestroyOnLoad"));
+        Destroy(GameObject.Find("RoomManager"));
+
+
+    }
+
 
 }
---
+++ Assets/countdownScript.cs
@@ -0,0 +1,71 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdownScript : MonoBehaviour
+{
+    [Header("Variables")]
+    bool startTimer = false;
+    double remainingTime;
+    double timerInc;
+    double startTime;
+    [SerializeField] double timer = 20;
+
+    [Header("Objects")]
+    public GameObject countdown;
+    public Text countdownText;
+
+    [Header("Hashmap")]
+    ExitGames.Client.Photon.Hashtable Sync;
+
+    void Start()
+    {
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            Time.timeScale = 0;
+
+            Sync = new ExitGames.Client.Photon.Hashtable();
+            startTime = PhotonNetwork.Time;
+            startTimer = true;
+            Sync.Add("StartTime", startTime);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(Sync);
+        }
+        else
+        {
+            startTime = double.Parse(PhotonNetwork.LocalPlayer.CustomProperties["StartTime"].ToString());
+            startTimer = true;
+        }
+    }
+
+    void Update()
+    {
+
+        if (!startTimer) return;
+
+        timeCalculations();
+
+        if (timerInc >= timer)
+        {
+            startGame();
+        }
+    }
+
+    void timeCalculations()
+    {
+        timerInc = PhotonNetwork.Time - startTime;
+
+        remainingTime = Mathf.Round((float)(timer - timerInc));
+
+        countdownText.text = remainingTime.ToString();
+    }
+
+    void startGame()
+    {
+        Time.timeScale = 1;
+        countdown.SetActive(false);
+    }
+
+}
+
